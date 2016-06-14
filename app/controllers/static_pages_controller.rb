bf41da8d1b7442cb6737c3349114c358787c95b1
Tinus <vertelmy@gmail.com>
2016-06-14 15:28:36 +1200

    added contact and about page as well as ActionMailer using SendGrid.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,14 @@ class StaticPagesController < ApplicationController
   def index
     @products = Product.limit(6)
   end
+
+  def thanks
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'tinus.wagnersa@gmail.com',
+      :subject => "A new message on Bootshop from #{@name}",
+      :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   get 'static_pages/contact'
-
   get 'static_pages/index'
+
+  post 'static_pages/thanks'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
