b17da61434f1ec85bcbd338ec99ef8cb6884c059
Tinus <vertelmy@gmail.com>
2016-06-15 15:17:15 +1200

    added user scaffold, defined model between user & orders and added devise gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :products
   get 'static_pages/about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613050346) do
+ActiveRecord::Schema.define(version: 20160615031334) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20160613050346) do
     t.datetime "updated_at",                         null: false
     t.string   "style"
     t.decimal  "price",      precision: 8, scale: 2
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
