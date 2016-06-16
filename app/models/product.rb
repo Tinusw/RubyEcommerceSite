d903f3f16bc5e17cfb9e2d30c9fc23a2ef049652
Tinus <vertelmy@gmail.com>
2016-06-16 16:21:45 +1200

    added form validation to new comments & new products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,11 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 
+  # data validation
+  validates :name, presence: true
+    validates :desc, presence: true
+    validates :price, numericality: { only_integer: true}
+
   # this is for arty.js and average score of a product
   def average_rating
     comments.average(:rating).to_f
