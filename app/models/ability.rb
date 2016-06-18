bf97796ab8a91392d3f4259766b9e54a9e868997
Tinus <vertelmy@gmail.com>
2016-06-18 17:12:19 +1200

    added admin flag to user schema and defined admin rights to remove comments and update products.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user(not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all 
+    else
+      can :manage, User, id: user.id
+    end
   end
 end
