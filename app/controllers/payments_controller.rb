51a6dbba762be51e64f72469842a8e814d9bb1de
Tinus <vertelmy@gmail.com>
2016-06-21 11:44:03 +1200

    created payments controller, and added create method.

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,22 @@
+class PaymentsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+    #Create charge on Stripe Servers.
+    begin
+      charge = Stripe::Charge::create(
+        :amount => @product.price * 100,
+        :currency => "usd",
+        :source => token
+        :desc => params[:stripeEmail]
+        )
+    rescue Stripe::CardError => e 
+      #Card was declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+  end
+    redirect_to product_path(@product)
+  end
+end
