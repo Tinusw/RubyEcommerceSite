e9700d2e5b9b9eac02398ec9c06df58d0bc38a4e
Tinus <vertelmy@gmail.com>
2016-06-21 11:30:50 +1200

    reworked product.price validation to allow 2 decimals and display prices accurately. Thusus allowing STRIPE to process prices correctly.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,8 @@ class Product < ApplicationRecord
 
   # data validation
   validates :name, presence: true
-    validates :desc, presence: true
-    validates :price, numericality: { only_integer: true}
-
+  validates :desc, presence: true
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
   # this is for arty.js and average score of a product
   def average_rating
     comments.average(:rating).to_f
