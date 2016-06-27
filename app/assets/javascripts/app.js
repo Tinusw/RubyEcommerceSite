ebf6a87fbb63e2bca4b68add71b8d7a314f21bd0
Tinus <vertelmy@gmail.com>
2016-06-27 15:49:25 +1200

    App reading from & to Rails API as intended. Added link_to in navbar if user is admin.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,31 +1,41 @@
-var app = angular.module('shop', []);
+var app = angular.module('shop', ['ngResource']);
 
 $(document).on('ready page:load', function() {
   angular.bootstrap(document.body, ['shop'])
 });
 
 // Model
-app.factory('models', function(){
+app.factory('models', ['$resource', function($resource){
+  var orders_model = $resource("/orders/:id.json", {id: "@id"});
+  var products_model = $resource("/products/:id.json", {id: "@id"});
+
   var x = {
-    order: []
+    orders: orders_model,
+    products: products_model
   };
   return x;
-});
+}]);
 
 // OrdersController Code
 app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
   // set orders = models.orders
-  $scope.orders = models.orders;
+  $scope.orders = models.orders.query();
+  $scope.products = models.products.query();
   
   // add an order
   $scope.addOrder = function(){
-    // if empty do not add to $scope.orders
-    if(!$scope.newOrder.product_id || $scope.newOrder.total === '') {return}
-    $scope.orders.push($scope.newOrder);
-  }
+    if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
+
+    order = models.orders.save($scope.newOrder, function(){
+      recent_order = models.orders.get({id: order.id});
+      $scope.orders.push(recent_order);
+      $scope.newOrder = '';
+    });
+  };
 
   // delete an order
   $scope.deleteOrder = function(order){
+    models.orders.delete(order);
     $scope.orders.splice($scope.orders.indexOf(order), 1);
   }
 }]);
