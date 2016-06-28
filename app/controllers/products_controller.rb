c71f165c2d9ee62f7fed7af4969b873a7802304c
Tinus <vertelmy@gmail.com>
2016-06-28 16:51:57 +1200

    sigh...

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-      @products = Product.where("name iLike ?", "%#{search_term}%")
+      @products = Product.where("name iLike ?", "%#{search_term}%").paginate(:page => params[:page], :per_page => 12)
       # return filtered search list
     else
       @products = Product.paginate(:page => params[:page], :per_page => 12)
